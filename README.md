b5c03c0b01a17ae42a787da74f38bda5dda8ab68
Gregory Hill <gregorydhill@outlook.com>
2021-08-18 15:14:04 +0100

    feat: import api and index
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

---
+++ README.md
@@ -0,0 +1,27 @@
+# interbtc-js
+
+## About
+
+The interbtc TypeScript library connects the Polkadot ecosystem with Bitcoin. It allows the creation of interBTC, a fungible token that represents Bitcoin in the Polkadot ecosystem. InterBTC is backed by Bitcoin 1:1 and allows redeeming of the equivalent amount of Bitcoins by relying on a collateralized third-party (Vaults).
+In comparison to other bridge constructions (like tBTC, wBTC, or RenVM) _anyone_ can become an intermediary by depositing collateral making interBTC the only truly open system.
+
+## License
+
+(C) Copyright 2021 [Interlay](https://www.interlay.io) Ltd
+
+interbtc-js is licensed under the terms of the Apache License (Version 2.0). See [LICENSE](LICENSE).
+
+## Contact
+
+Website: [Interlay.io](https://www.interlay.io)
+
+Twitter: [@interlayHQ](https://twitter.com/InterlayHQ)
+
+Email: contact@interlay.io
+
+## Acknowledgements
+
+We would like to thank the following teams for their continuous support:
+
+-   [Web3 Foundation](https://web3.foundation/)
+-   [Parity Technologies](https://www.parity.io/)
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { BitcoinNetwork, createInterbtcAPI, InterBTCAPI } from "@interlay/interbtc-api";
+import { AddressOrPair } from "@polkadot/api/types";
+import { DefaultIndexAPI, WrappedIndexAPI } from "./interbtcIndex";
+import { ApiPromise } from "@polkadot/api";
+
+export const INDEX_LOCAL_URL = "http://localhost:3007";
+
+export async function createInterbtc(
+    endpoint: string,
+    network?: BitcoinNetwork,
+    indexEndpoint?: string,
+    account?: AddressOrPair,
+    autoConnect?: number | false | undefined
+): Promise<InterBtc> {
+    const interBtcApi = await createInterbtcAPI(endpoint, network, account, autoConnect);
+    const polkadotApi = interBtcApi.api;
+    const interBtcIndex = DefaultIndexAPI({ basePath: indexEndpoint }, polkadotApi);
+    return new DefaultInterBtc(polkadotApi, interBtcApi, interBtcIndex);
+}
+
+export interface InterBtc {
+    readonly polkadotApi: ApiPromise;
+    readonly interBtcApi: InterBTCAPI;
+    readonly interBtcIndex: WrappedIndexAPI;
+}
+
+export class DefaultInterBtc implements InterBtc {
+    public readonly polkadotApi: ApiPromise;
+    public readonly interBtcApi: InterBTCAPI;
+    public readonly interBtcIndex: WrappedIndexAPI;
+
+    constructor(
+        polkadotApi: ApiPromise,
+        interBtcApi: InterBTCAPI,
+        interBtcIndex: WrappedIndexAPI,
+
+    ) {
+        this.polkadotApi = polkadotApi;
+        this.interBtcApi = interBtcApi;
+        this.interBtcIndex = interBtcIndex;
+    }
+}
